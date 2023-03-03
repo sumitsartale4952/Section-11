6274045947fc9d815af3194eeb9af1ca1beb9124
sumitsartale4952 <126549272+sumitsartale4952@users.noreply.github.com>
2023-03-03 18:15:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import  { Fragment } from "react";
+import Header from "./componets/Layout/Header";
+import Meals from "./componets/Meals/Meals";
+
+import Cart from "./componets/Cart/Cart";
+
+function App()
+{
+  return(
+    <Fragment>
+      <Cart/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
